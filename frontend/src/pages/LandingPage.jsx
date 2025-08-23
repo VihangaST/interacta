776d60dd67b01e7975c03acb416cac5c89141872
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-23 19:55:21 +0530

    Landing page created

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import Form from "../components/Form";
+
+function LandingPage() {
+  return (
+    <>
+      <main className="min-vh-100 bg-secondary-subtle">
+        <div className="d-flex justify-content-center">
+          <Form />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default LandingPage;
