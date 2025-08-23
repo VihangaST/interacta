74730c8d12dde12d5be85909158cae6ab731e860
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-23 19:55:35 +0530

    Question page added

---
+++ frontend/src/pages/QuestionPage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+function QuestionPage() {
+  useEffect(() => {
+    // Your effect logic here
+  }, []);
+  return (
+    <>
+      <main className="min-vh-100 bg-secondary-subtle">
+        <h1>Question Page</h1>
+      </main>
+    </>
+  );
+}
+
+export default QuestionPage;
