091399dd0fac3f01a2dec20e793ab2bf7045e3f5
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-24 17:11:29 +0530

    bug fix- generate questuns and store as a array of objects

--- frontend/src/pages/QuestionPage.jsx
+++ frontend/src/pages/QuestionPage.jsx
@@ -1,13 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Question from "../components/Question";
+import axios from "axios";
 
 function QuestionPage() {
+  const [questionsList, setQuestionsList] = useState([]);
+  const [questionCount, setQuestionCount] = useState(0);
   useEffect(() => {
-    // Your effect logic here
+    axios
+      .get("http://localhost:3000/api/questions")
+      .then((res) => {
+        console.log("Questions fetched successfully");
+        setQuestionsList(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get("http://localhost:3000/api/questions/questionCount")
+      .then((res) => {
+        console.log(res.body);
+        setQuestionCount(res.data.questionCount);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   return (
     <>
       <main className="min-vh-100 bg-secondary-subtle">
-        <h1>Question Page</h1>
+        <div>
+          <h1>Question Page</h1>
+          {questionsList.map((question) => (
+            <Question questionData={question} />
+          ))}
+        </div>
+        <div className="container text-center">
+          {Array.from({ length: questionCount }, (_, idx) => (
+            <button
+              className={`btn col fs-1 border rounded border-5 border-danger m-2 bg-danger-subtle fw-bold`}
+              style={{ width: "100px", height: "100px" }}
+            >
+              {" "}
+              Q {idx}
+            </button>
+          ))}
+        </div>
       </main>
     </>
   );
