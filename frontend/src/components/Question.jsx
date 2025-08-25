eb9d03ff00baf2b57e3a988738f12ef98a01fb05
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-08-25 13:36:56 +0530

    Question component added

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,27 +1,63 @@
-function Question(prop) {
-  const questionData = prop.questionData;
+import React, { useState } from "react";
+
+function Question({ questionData, answeredQuestions, setAnsweredQuestions }) {
   const question = questionData.question;
   const options = questionData.options;
 
+  const [selectedOption, setSelectedOption] = React.useState("");
+  function handleSubmit(selectedOption) {
+    if (!selectedOption) {
+      alert("Please select an option before submitting.");
+      return;
+    }
+    if (selectedOption === questionData.correctAnswer) {
+      alert("Correct answer!");
+    } else {
+      alert(
+        `Wrong answer! The correct answer is: ${questionData.correctAnswer}`
+      );
+    }
+  }
+
   return (
     <>
-      <h3>{question.replace(/\*/g, "")}</h3>
-      {options.map((option, index) => {
-        return (
-          <div class="form-check" key={index}>
-            <input
-              class="form-check-input"
-              type="radio"
-              name="radioDefault"
-              value={option}
-              id="radioDefault1"
-            />
-            <label class="form-check-label" for="radioDefault1">
-              {option}
-            </label>
-          </div>
-        );
-      })}
+      <div className="container border rounded border-5 border-primary p-3 mb-2 text-danger-emphasis">
+        <h5 className="font-monospace mb-4">{question.replace(/\*/g, "")}</h5>
+        <div className="m-2">
+          {options.map((option, index) => {
+            return (
+              <div className="form-check m-2" key={index}>
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="radioDefault"
+                  value={option}
+                  id={`radioDefault${index}`}
+                  onChange={() => {
+                    setSelectedOption(option);
+                  }}
+                />
+                <label
+                  className="form-check-label font-monospace"
+                  htmlFor={`radioDefault${index}`}
+                >
+                  {option}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          onClick={() => {
+            handleSubmit(selectedOption);
+            setAnsweredQuestions(answeredQuestions + 1);
+            console.log("answeredQuestions", answeredQuestions);
+          }}
+          className={`btn font-monospace btn-primary col fs-6`}
+        >
+          submit
+        </button>
+      </div>
     </>
   );
 }
